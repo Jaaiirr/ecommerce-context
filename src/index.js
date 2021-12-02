22f2f5296a9f271ef5fcbba5e975e425c3858d83
Jaaiirr <jairtellezromero9678@gmail.com>
2021-12-01 21:48:38 -0600

    Fixed Header

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Bootswatch
+import "bootswatch/dist/quartz/bootstrap.min.css";
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
